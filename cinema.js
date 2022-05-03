4205dc52079c4814d8ea80557a11f5eb61c1935e
DaianaajFerrreira <daia.jf@hotmail.com>
2022-05-03 12:08:25 -0300

    Importando e manipulando JSON

--- cinema.js
+++ cinema.js
@@ -1,1 +1,13 @@
-console.log("criamos o arquivo")
+
+// puxar o json dentro de uma variavel
+var catalogo = require ("./database/catalogo.json");
+
+console.log(catalogo)
+
+//converte JSON objeto para -   em String para tirar tabulações desnecessarias
+var catalogoString = JSON.stringify(catalogo);
+console.log(catalogoString)
+
+//converter JSON  como estring, para objeto 
+var catalogoObj = JSON.parse(catalogoString)
+console.log (catalogoObj)
